c548da1b26dc276e2336e134024a7e35d979f60d
psevdocoder <mr.inco9ni7o@gmail.com>
2022-04-14 14:16:24 +0300

    some fixes to prevent illegal actions with database by Dima Lisyutin

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from flaskext.mysql import MySQL
 
 mysql = MySQL()
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'S3cret'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'Secret1'
 app.config['MYSQL_DATABASE_DB'] = 'project'
 app.config['MYSQL_DATABASE_HOST'] = '185.18.55.107'
 mysql.init_app(app)
